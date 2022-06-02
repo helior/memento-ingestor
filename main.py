c80bb7d40ccf02bbf6bf5e5cab5d98d4ec1d6dde
Helior Colorado <me@helior.info>
2022-06-02 07:09:08 -0700

    Move handlers into their own module

--- main.py
+++ main.py
@@ -1,69 +1,8 @@
-import datetime
 import time
-import os
-from dateutil import parser
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-import requests
-from src.probe import ffprobe
 
-
-
-def on_created(event):
-  """
-  On Create:
-  TODO: info log a new file (event.ingestor.fileDetected, path)
-  """
-  print(f"Created: {event.src_path} ")
-  # TODO: Log event
-
-  try:
-    # Probe
-    probe = ffprobe(event.src_path)
-
-    #Process
-    created_time = parser.parse(probe.get('format').get('tags').get('creation_time'))
-    duration = probe.get('format').get('duration')
-    start_time = created_time - datetime.timedelta(seconds=float(duration))
-
-    # Move
-    processed_path = './processed' # TODO: or configured path
-    basename = os.path.basename(event.src_path)
-    destination_path = os.path.abspath(os.path.join(processed_path, basename))
-    os.rename(event.src_path, destination_path)
-
-    # Save
-    data = {
-      "label": basename,
-      "startTime": start_time,
-      "duration": duration,
-    }
-    files = {'media': open (destination_path, 'rb')}
-    r = requests.post('http://localhost:8000/api/momentoriginalaudio/', files=files, data=data)
-
-    # Log successful completion
-    print(probe)
-    print(r.json())
-  except Exception as e:
-    print(e)
-
-def on_deleted(event):
-  """
-  On Delete:
-  TODO: info log manual deletion of a file (event.ingestor.fileDeleted, path)
-  """
-  print(f"Deleted: {event.src_path} ")
-
-def on_modified(event):
-
-  print(f"Modified: {event.src_path} ")
-
-def on_moved(event):
-  """
-  On Move:
-  - TODO: info log event (event.ingestor.fileMoved, path, MID)
-  """
-  print(f"Moved: {event.src_path} → {event.dest_path} ")
+from src.handlers import on_created, on_deleted, on_modified, on_moved
 
 
 if __name__ == '__main__':
@@ -75,7 +14,7 @@ if __name__ == '__main__':
   """
 
   # Config for WATCH FOLDER
-  patterns = ['*.mp3', '*.mp4', '*.m4a']
+  patterns = ['*.mp3', '*.mp4', '*.m4a'] # TODO: or from configuration
   ignore_patterns = None
   ignore_directories = False
   case_sensitive = True
@@ -86,10 +25,10 @@ if __name__ == '__main__':
   event_handler.on_modified = on_modified
   event_handler.on_moved = on_moved
 
-  path = './watchfolder' # TODO: or configured path
-  go_recursively = True
+  watchfolder_path = './watchfolder' # TODO: or from configuration
+  go_recursively = False
   observer = Observer()
-  observer.schedule(event_handler, path, go_recursively)
+  observer.schedule(event_handler, watchfolder_path, go_recursively)
 
   # Begin watching..
   print('event.app.start')
---
+++ src/handlers.py
@@ -0,0 +1,62 @@
+import datetime
+from dateutil import parser
+import requests
+import os
+
+from src.probe import ffprobe
+
+def on_created(event):
+  """
+  On Create:
+  TODO: info log a new file (event.ingestor.fileDetected, path)
+  """
+  print(f"Created: {event.src_path} ")
+  # TODO: Log event
+
+  try:
+    # Probe
+    probe = ffprobe(event.src_path)
+
+    #Process
+    created_time = parser.parse(probe.get('format').get('tags').get('creation_time'))
+    duration = probe.get('format').get('duration')
+    start_time = created_time - datetime.timedelta(seconds=float(duration))
+
+    # Move
+    processed_path = './processed' # TODO: or from configuration
+    basename = os.path.basename(event.src_path)
+    destination_path = os.path.abspath(os.path.join(processed_path, basename))
+    os.rename(event.src_path, destination_path)
+
+    # Save
+    data = {
+      "label": basename,
+      "startTime": start_time,
+      "duration": duration,
+    }
+    files = {'media': open (destination_path, 'rb')}
+    r = requests.post('http://localhost:8000/api/momentoriginalaudio/', files=files, data=data)
+
+    # Log successful completion
+    print(probe)
+    print(r.json())
+  except Exception as e:
+    print(e)
+
+def on_deleted(event):
+  """
+  On Delete:
+  TODO: info log manual deletion of a file (event.ingestor.fileDeleted, path)
+  """
+  print(f"Deleted: {event.src_path} ")
+
+def on_modified(event):
+
+  print(f"Modified: {event.src_path} ")
+
+def on_moved(event):
+  """
+  On Move:
+  - TODO: info log event (event.ingestor.fileMoved, path, MID)
+  """
+  print(f"Moved: {event.src_path} → {event.dest_path} ")
